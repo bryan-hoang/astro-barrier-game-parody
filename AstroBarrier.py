b09d8d355f01ca7aa1f8f32e5868f857cfef4004
Bryan Hoang <Bryan.C.Hoang@gmail.com>
2018-12-01 11:40:02 -0500

    Add python file and textures folder

---
+++ AstroBarrier.py
@@ -0,0 +1,8 @@
+import arcade
+
+arcade.window_commands.open_window(800, 600, 'Astro Barrier', False)
+arcade.set_background_color(arcade.color.GREEN)
+arcade.start_render()
+arcade.finish_render()
+# arcade.quick_run(3)
+arcade.window_commands.run()
