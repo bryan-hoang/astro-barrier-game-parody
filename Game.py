75b81293f588f3fe5474e76918721cc54d13472b
Bryan Hoang <Bryan.C.Hoang@gmail.com>
2018-12-01 16:41:59 -0500

    Add player movement based on inputs

--- Game.py
+++ Game.py
@@ -13,40 +13,68 @@ SCREEN_HEIGHT = 600
 SPRITE_SCALING_SHIP = 0.05
 SPRITE_SCALING_TARGET = 0.8
 SPRITE_SCALING_BULLET = 0.8
+
 TARGET_COUNT = 1
+
+MOVEMENT_SPEED = 5
+
 
 class gameState(Enum):
     MAIN_MENU = 0
     PLAYING = 1
     GAME_OVER = 2
 
+
+class Player(arcade.Sprite):
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+
 class AstroBarrier(arcade.Window):
     def __init__(self):
-        # initialize window
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, title="Club Penguin - Astro Barrier")
 
-        self.player = None
+        # Call the parent class initializer
+        super().__init__(
+            SCREEN_WIDTH, SCREEN_HEIGHT, title="Club Penguin - Astro Barrier")
+
+        self.player_list = None
+
+        self.player_sprite = None
+        self.level = 1
+
         self.target_sprites = None
         self.bullet_sprites = None
         self.shoot = None
+
+        # Track the current state of what key is pressed
+        self.left_pressed = False
+        self.right_pressed = False
+
         # use Bryan's code for setting up window
         arcade.set_background_color(arcade.color.LIGHT_GREEN)
 
     def setup(self):
 
-        self.level = 1
-
-        self.player = arcade.SpriteList()
+        self.player_list = arcade.SpriteList()
         self.target_sprites = arcade.SpriteList()
         self.bullet_sprites = arcade.SpriteList()
         self.shoot = False
         # use more of Bryan's code #hypercarry
 
-        self.player_sprite = arcade.Sprite(
+        self.level = 1
+
+        self.player_sprite = Player(
             "textures/Astro_Barrier_Ship_pin.png", SPRITE_SCALING_SHIP)
         self.player_sprite.center_x = 400
         self.player_sprite.center_y = 40
-        self.player.append(self.player_sprite)
+        self.player_list.append(self.player_sprite)
 
         # Create the targets
         for i in range(TARGET_COUNT):
@@ -69,19 +97,6 @@ class AstroBarrier(arcade.Window):
         # TODO: Eden's code for initializing player
         # self.player = Player()
 
-   # initialize a shit ton of variables
-
-        def on_key_press(self, symbol, modifiers):
-            # check for user input
-            if symbol == arcade.key.SPACE:
-                self.shoot = True
-
-            if symbol == arcade.key.RIGHT:
-                self.moveRight = True
-
-            if symbol == arcade.key.LEFT:
-                self.moveLeft = True
-
     # Draw the sprites
     def on_draw(self):
         """
@@ -93,18 +108,28 @@ class AstroBarrier(arcade.Window):
 
         # Draw all the sprites.
 
-        self.player.draw()
+        self.player_list.draw()
         self.target_sprites.draw()
         self.bullet_sprites.draw()
 
         # Render the text
-        arcade.draw_text(f"Score: {self.score}", 10,
+        arcade.draw_text(f"Score: {self.level}", 10,
                          20, arcade.color.WHITE, 14)
-        arcade.draw_text(f"Level: {self.level}", 10,
-                         40, arcade.color.WHITE, 14)
 
-    def on_update(self, delta_time):
+    def update(self, delta_time):
         """ Movement and game logic """
+
+        # Calculate speed based on the keys pressed
+        self.player_sprite.change_x = 0
+        self.player_sprite.change_y = 0
+
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+        # Call update on player
+        self.player_list.update()
 
         # Call update on bullet sprites
         self.bullet_sprites.update()
@@ -124,6 +149,23 @@ class AstroBarrier(arcade.Window):
             # If the bullet flies off-screen, remove it.
             if bullet.bottom > SCREEN_HEIGHT:
                 bullet.kill()
+
+    # initialize a shit ton of variables
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
 
 
 def main():
